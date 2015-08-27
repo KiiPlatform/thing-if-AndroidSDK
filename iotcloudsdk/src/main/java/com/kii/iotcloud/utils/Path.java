79742551a99d2f4baa12d9e80069dd3f86412531
nfukuzaki <noriyoshi.fukuzaki@kii.com>
2015-08-27 14:11:28 +0900

    added javadoc

--- iotcloudsdk/src/main/java/com/kii/iotcloud/utils/Path.java
+++ iotcloudsdk/src/main/java/com/kii/iotcloud/utils/Path.java
@@ -3,6 +3,9 @@ package com.kii.iotcloud.utils;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
+/**
+ * Performs operations on String instances that contain URL path information.
+ */
 public class Path {
     public static String combine(@Nullable String path1, @Nullable String path2) {
         if (TextUtils.isEmpty(path1)) {
