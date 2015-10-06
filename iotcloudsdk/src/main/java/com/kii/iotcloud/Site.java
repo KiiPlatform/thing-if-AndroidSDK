f4d4f9716c61114e92637ee1f8f7fe8bb7021354
Yongping Chen <yongping.chen@kii.com>
2015-10-06 19:18:07 +0900

    chang CN to CN3

--- iotcloudsdk/src/main/java/com/kii/iotcloud/Site.java
+++ iotcloudsdk/src/main/java/com/kii/iotcloud/Site.java
@@ -3,7 +3,7 @@ package com.kii.iotcloud;
 public enum Site {
     US("https://api.kii.com"),
     JP("https://api-jp.kii.com"),
-    CN("https://api-cn2.kii.com"),
+    CN3("https://api-cn3.kii.com"),
     SG("https://api-sg.kii.com");
 
     private final String baseUrl;
