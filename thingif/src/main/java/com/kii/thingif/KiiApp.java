03feefadb8d4783c2eb2631199df1a680bb891fd
satoshi <satoshi.kumano@kii.com>
2015-12-14 14:13:53 +0900

    Add site name in KiiApp.

--- thingif/src/main/java/com/kii/thingif/KiiApp.java
+++ thingif/src/main/java/com/kii/thingif/KiiApp.java
@@ -10,6 +10,7 @@ public class KiiApp {
     private String appKey;
     private String hostName;
     private String baseUrl;
+    private String siteName;
 
 
     /** Instantiate Kii App with App Location.
@@ -34,6 +35,7 @@ public class KiiApp {
         this.appKey = appKey;
         this.hostName = site.getHostName();
         this.baseUrl = "https://" + this.hostName;
+        this.siteName = site.name();
     }
 
     /** Instantiate Kii App with Host Name.
@@ -41,6 +43,7 @@ public class KiiApp {
      * Will use this constructor to instantiate KiiApp.
      * (Private/ Dedicated location is only available in Enterprise subscription.)
      * If you're using public Kii Cloud, please use {@link KiiApp(String, String, Site)}
+     * With this constructor, {@link #getSiteName()} returns fixed string "CUSTOM".
      * @param appID ID of the app.
      * @param appKey Key of the app.
      * @param hostName Host Name of the app.
@@ -60,6 +63,36 @@ public class KiiApp {
         this.appKey = appKey;
         this.hostName = hostName;
         this.baseUrl = "https://" + hostName;
+        this.siteName = "CUSTOM";
+    }
+
+    /** Instantiate Kii App with Host Name and Site Name.
+     * Who host Kii Cloud in private/ dedicated location
+     * Will use this constructor to instantiate KiiApp.
+     * (Private/ Dedicated location is only available in Enterprise subscription.)
+     * If you're using public Kii Cloud, please use {@link KiiApp(String, String, Site)}
+     * siteName is used by Gateway Agent to resolve the server location stored with the siteName
+     * in the Gateway configuration.
+     * @param appID ID of the app.
+     * @param appKey Key of the app.
+     * @param hostName Host name of the app.
+     * @param siteName Site name of the app. (Corresponds to Gateway Agent configuration.)
+     */
+    public KiiApp(String appID, String appKey, String hostName, String siteName) {
+        if (TextUtils.isEmpty(appID)) {
+            throw new IllegalArgumentException("appID is null or empty.");
+        }
+        if (TextUtils.isEmpty(appKey)) {
+            throw new IllegalArgumentException("appKey is null or empty.");
+        }
+        if (TextUtils.isEmpty(hostName)) {
+            throw new IllegalArgumentException("hostName is null or empty.");
+        }
+        this.appID = appID;
+        this.appKey = appKey;
+        this.hostName = hostName;
+        this.baseUrl = "https://" + hostName;
+        this.siteName = siteName;
     }
 
     public String getAppID() {
@@ -76,6 +109,10 @@ public class KiiApp {
 
     public String getBaseUrl() {
         return this.baseUrl;
+    }
+
+    public String getSiteName() {
+        return this.siteName;
     }
 
 }
