34e61ec4d9a4f311b35823d0788bf46c60ab377f
satoshi <satoshi.kumano@kii.com>
2015-12-25 17:22:54 +0900

    Fixed small test.

--- thingif/src/androidTest/java/com/kii/thingif/GsonSerializationTest.java
+++ thingif/src/androidTest/java/com/kii/thingif/GsonSerializationTest.java
@@ -440,7 +440,7 @@ public class GsonSerializationTest extends SmallTestBase {
                         "        \"target\":\"thing:1234\"" +
                         "    }," +
                         "    \"predicate\":{" +
-                        "        \"eventSource\":\"STATES\"," +
+                        "        \"eventSource\":\"states\"," +
                         "        \"triggersWhen\":\"CONDITION_FALSE_TO_TRUE\"," +
                         "        \"condition\":{" +
                         "            \"type\":\"and\"," +
