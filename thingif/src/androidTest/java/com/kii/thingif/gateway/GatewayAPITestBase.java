cbb04434e0120923180898a5a24ec98daa4751ea
Noriyoshi Fukuzaki <burton9999dev@gmail.com>
2016-03-23 14:50:16 +0900

    added small tests for login

--- thingif/src/androidTest/java/com/kii/thingif/gateway/GatewayAPITestBase.java
+++ thingif/src/androidTest/java/com/kii/thingif/gateway/GatewayAPITestBase.java
@@ -2,15 +2,29 @@ package com.kii.thingif.gateway;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.kii.thingif.KiiApp;
 import com.kii.thingif.SmallTestBase;
 import com.squareup.okhttp.mockwebserver.MockResponse;
 import com.squareup.okhttp.mockwebserver.MockWebServer;
+import com.squareup.okhttp.mockwebserver.RecordedRequest;
 
+import org.json.JSONObject;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class GatewayAPITestBase extends SmallTestBase {
+
+    protected static final String APP_ID = "smalltest";
+    protected static final String APP_KEY = "abcdefghijklmnopqrstuvwxyz123456789";
+    private static final String SDK_VERSION = "0.10.0";
     protected MockWebServer server;
 
     @Before
@@ -40,5 +54,52 @@ public class GatewayAPITestBase extends SmallTestBase {
         responseBody.addProperty("accessToken", accessToken);
         this.server.enqueue(new MockResponse().setResponseCode(httpStatus).setBody(responseBody.toString()));
     }
+    protected void assertRequestBody(String expected, RecordedRequest actual) {
+        this.assertRequestBody(new JsonParser().parse(expected), actual);
+    }
+    protected void assertRequestBody(JSONObject expected, RecordedRequest actual) {
+        this.assertRequestBody(new JsonParser().parse(expected.toString()), actual);
+    }
+    protected void assertRequestBody(JsonElement expected, RecordedRequest actual) {
+        Assert.assertEquals("request body", expected, new JsonParser().parse(actual.getBody().readUtf8()));
+    }
 
+    /**
+     * Utilities of checking request header.
+     * Don't include X-Kii-SDK header in expected param and don't remove it from
+     * actual param.
+     * @param expected
+     * @param actual
+     */
+    protected void assertRequestHeader(Map<String, String> expected, RecordedRequest actual) {
+        Map<String, List<String>> actualMap = new HashMap<String, List<String>>();
+        for (String headerName : actual.getHeaders().names()) {
+            actualMap.put(headerName, actual.getHeaders().values(headerName));
+        }
+        // following headers are added by OkHttp client automatically. So we need to ignore them.
+        actualMap.remove("Content-Length");
+        actualMap.remove("Host");
+        actualMap.remove("Connection");
+        actualMap.remove("Accept-Encoding");
+        actualMap.remove("User-Agent");
+
+        // Check X-Kii-SDK Header
+        List<String> kiiSDK = actualMap.remove("X-Kii-SDK");
+        Assert.assertEquals(1, kiiSDK.size());
+        Pattern p = Pattern.compile("sn=at;sv=" + SDK_VERSION + ";pv=\\d*");
+        Matcher m = p.matcher(kiiSDK.get(0));
+        Assert.assertTrue(m.matches());
+
+        Assert.assertEquals("number of request headers", expected.size(), actualMap.size());
+        for (Map.Entry<String, String> h : expected.entrySet()) {
+            String expectedHeaderValue = h.getValue();
+            if ("Content-Type".equalsIgnoreCase(h.getKey())) {
+                // OkHttp adds charset to the Content-Type automatically.
+                if (expectedHeaderValue.indexOf("; charset=utf-8") < 0) {
+                    expectedHeaderValue += "; charset=utf-8";
+                }
+            }
+            Assert.assertEquals("request header(" + h.getKey() + ")", expectedHeaderValue, actualMap.get(h.getKey()).get(0));
+        }
+    }
 }
