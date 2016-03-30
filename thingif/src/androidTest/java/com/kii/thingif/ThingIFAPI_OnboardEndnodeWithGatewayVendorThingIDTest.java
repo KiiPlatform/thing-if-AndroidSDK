5d122496ea95f74e20e5a0a26097a2b51568d6c1
nfukuzaki <noriyoshi.fukuzaki@kii.com>
2016-03-30 15:56:35 +0900

    added small tests template

---
+++ thingif/src/androidTest/java/com/kii/thingif/ThingIFAPI_OnboardEndnodeWithGatewayVendorThingIDTest.java
@@ -0,0 +1,9 @@
+package com.kii.thingif;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ThingIFAPI_OnboardEndnodeWithGatewayVendorThingIDTest extends ThingIFAPITestBase {
+}
