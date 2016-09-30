e881b1fc8864ff21f82d48a93122a16eb65247f9
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2016-09-30 15:11:53 +0900

    Value overwrite test is added to TriggerOptionsTest.

--- thingif/src/androidTest/java/com/kii/thingif/trigger/TriggerOptionsTest.java
+++ thingif/src/androidTest/java/com/kii/thingif/trigger/TriggerOptionsTest.java
@@ -179,4 +179,45 @@ public class TriggerOptionsTest extends SmallTestBase {
         }
     }
 
+    @Test
+    public void valueOverwriteTest() throws Exception {
+        String title1 = "title1";
+        String description1 = "description1";
+        JSONObject metadata1 = new JSONObject();
+        metadata1.put("key1", "value1");
+
+        TriggerOptions.Builder builder = TriggerOptions.Builder.builder();
+
+        builder.setTitle(title1).setDescription(description1).
+                setMetadata(metadata1);
+
+        Assert.assertEquals(title1, builder.getTitle());
+        Assert.assertEquals(description1, builder.getDescription());
+        assertJSONObject(metadata1, builder.getMetadata());
+
+        TriggerOptions options = builder.build();
+
+        Assert.assertEquals(title1, options.getTitle());
+        Assert.assertEquals(description1, options.getDescription());
+        assertJSONObject(metadata1, options.getMetadata());
+
+
+        String title2 = "title2";
+        String description2 = "description2";
+        JSONObject metadata2 = new JSONObject();
+        metadata2.put("key2", "value2");
+
+        builder.setTitle(title2).setDescription(description2).
+                setMetadata(metadata2);
+
+        Assert.assertEquals(title2, builder.getTitle());
+        Assert.assertEquals(description2, builder.getDescription());
+        assertJSONObject(metadata2, builder.getMetadata());
+
+        options = builder.build();
+
+        Assert.assertEquals(title2, options.getTitle());
+        Assert.assertEquals(description2, options.getDescription());
+        assertJSONObject(metadata2, options.getMetadata());
+    }
 }
