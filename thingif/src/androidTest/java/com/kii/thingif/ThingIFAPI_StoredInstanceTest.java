cdef5ace766e0ebe46113b1374c7c166cf9f065a
naoki.akimoto <naoki.akimoto@muraoka-design.com>
2016-10-28 18:54:49 +0900

    add tests.

--- thingif/src/androidTest/java/com/kii/thingif/ThingIFAPI_StoredInstanceTest.java
+++ thingif/src/androidTest/java/com/kii/thingif/ThingIFAPI_StoredInstanceTest.java
@@ -1,5 +1,7 @@
 package com.kii.thingif;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -92,5 +94,97 @@ public class ThingIFAPI_StoredInstanceTest extends ThingIFAPITestBase {
     @Test(expected = StoredThingIFAPIInstanceNotFoundException.class)
     public void loadFromStoredInstanceWithoutStoredInstanceTest() throws Exception {
         ThingIFAPI.loadFromStoredInstance(InstrumentationRegistry.getTargetContext());
+    }
+
+    @Test(expected = StoredThingIFAPIInstanceNotFoundException.class)
+    public void loadFromStoredInstanceNoSDKVersionTest() throws Exception {
+        String vendorThingID = "v1234567890abcde";
+        String thingPassword = "password";
+        JSONObject thingProperties = new JSONObject();
+        thingProperties.put("manufacturer", "Kii");
+        String thingID = "th.1234567890";
+        String accessToken = "thing-access-token-1234";
+        this.addMockResponseForOnBoard(200, thingID, accessToken);
+
+        ThingIFAPI api = this.createThingIFAPIWithDemoSchema(APP_ID, APP_KEY);
+        Assert.assertFalse(api.onboarded());
+        Target target = api.onboard(vendorThingID, thingPassword, DEMO_THING_TYPE, thingProperties);
+        Assert.assertTrue(api.onboarded());
+
+        Context context = InstrumentationRegistry.getTargetContext();
+        SharedPreferences preferences = context.getSharedPreferences(
+                "com.kii.thingif.preferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.remove("ThingIFAPI_VERSION");
+        editor.apply();
+
+        ThingIFAPI.loadFromStoredInstance(context);
+    }
+
+    @Test(expected = StoredThingIFAPIInstanceNotFoundException.class)
+    public void loadFromStoredInstanceLowerSDKVersionTest() throws Exception {
+        String vendorThingID = "v1234567890abcde";
+        String thingPassword = "password";
+        JSONObject thingProperties = new JSONObject();
+        thingProperties.put("manufacturer", "Kii");
+        String thingID = "th.1234567890";
+        String accessToken = "thing-access-token-1234";
+        this.addMockResponseForOnBoard(200, thingID, accessToken);
+
+        ThingIFAPI api = this.createThingIFAPIWithDemoSchema(APP_ID, APP_KEY);
+        Assert.assertFalse(api.onboarded());
+        Target target = api.onboard(vendorThingID, thingPassword, DEMO_THING_TYPE, thingProperties);
+        Assert.assertTrue(api.onboarded());
+
+        Context context = InstrumentationRegistry.getTargetContext();
+        SharedPreferences preferences = context.getSharedPreferences(
+                "com.kii.thingif.preferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("ThingIFAPI_VERSION", "0.0.0");
+        editor.apply();
+
+        ThingIFAPI.loadFromStoredInstance(context);
+    }
+
+    @Test
+    public void loadFromStoredInstanceUpperSDKVersionTest() throws Exception {
+        String vendorThingID = "v1234567890abcde";
+        String thingPassword = "password";
+        JSONObject thingProperties = new JSONObject();
+        thingProperties.put("manufacturer", "Kii");
+        String thingID = "th.1234567890";
+        String accessToken = "thing-access-token-1234";
+        this.addMockResponseForOnBoard(200, thingID, accessToken);
+
+        ThingIFAPI api = this.createThingIFAPIWithDemoSchema(APP_ID, APP_KEY);
+        Assert.assertFalse(api.onboarded());
+        Target target = api.onboard(vendorThingID, thingPassword, DEMO_THING_TYPE, thingProperties);
+        Assert.assertTrue(api.onboarded());
+
+        Context context = InstrumentationRegistry.getTargetContext();
+        SharedPreferences preferences = context.getSharedPreferences(
+                "com.kii.thingif.preferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("ThingIFAPI_VERSION", "1000.0.0");
+        editor.apply();
+
+        ThingIFAPI restoredApi = ThingIFAPI.loadFromStoredInstance(context);
+
+        assertEquals(api.getAppID(), restoredApi.getAppID());
+        assertEquals(api.getAppKey(), restoredApi.getAppKey());
+        assertEquals(api.getBaseUrl(), restoredApi.getBaseUrl());
+        assertEquals(api.getOwner().getTypedID(), restoredApi.getOwner().getTypedID());
+        assertEquals(api.getOwner().getAccessToken(), restoredApi.getOwner().getAccessToken());
+        assertEquals(api.getTarget().getTypedID(), restoredApi.getTarget().getTypedID());
+        assertEquals(api.getTarget().getAccessToken(), restoredApi.getTarget().getAccessToken());
+        assertEquals(new JsonParser().parse(GsonRepository.gson().toJson(api)),
+                new JsonParser().parse(GsonRepository.gson().toJson(restoredApi)));
+
+        ThingIFAPI.removeAllStoredInstances();
+        try {
+            ThingIFAPI.loadFromStoredInstance(InstrumentationRegistry.getTargetContext(), "ThingB");
+            fail("StoredThingIFAPIInstanceNotFoundException should be thrown");
+        } catch (StoredThingIFAPIInstanceNotFoundException e) {
+        }
     }
 }
--- thingif/src/androidTest/java/com/kii/thingif/gateway/GatewayAPI_StoredInstanceTest.java
+++ thingif/src/androidTest/java/com/kii/thingif/gateway/GatewayAPI_StoredInstanceTest.java
@@ -1,6 +1,7 @@
 package com.kii.thingif.gateway;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
@@ -82,4 +83,82 @@ public class GatewayAPI_StoredInstanceTest extends GatewayAPITestBase {
         GatewayAPI.loadFromStoredInstance(InstrumentationRegistry.getTargetContext());
     }
 
+    @Test(expected = StoredGatewayAPIInstanceNotFoundException.class)
+    public void loadFromStoredInstanceNoSDKVersionTest() throws Exception {
+        String username = "user01";
+        String password = "pa$$word";
+        String accessToken = "token-abcd1234";
+        KiiApp app = this.getApp(APP_ID, APP_KEY);
+        Uri gatewayAddress = getGatewayAddress();
+        Context context = InstrumentationRegistry.getTargetContext();
+        GatewayAPIBuilder builder = GatewayAPIBuilder.newBuilder(context, app, gatewayAddress);
+        GatewayAPI api = builder.build();
+
+        this.addMockResponseForLogin(200, accessToken);
+        api.login(username, password);
+        Assert.assertEquals(accessToken, api.getAccessToken());
+
+        SharedPreferences preferences = context.getSharedPreferences(
+                "com.kii.thingif.preferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.remove("GatewayAPI_VERSION");
+        editor.apply();
+
+        GatewayAPI.loadFromStoredInstance(context);
+    }
+
+    @Test(expected = StoredGatewayAPIInstanceNotFoundException.class)
+    public void loadFromStoredInstanceLowerSDKVersionTest() throws Exception {
+        String username = "user01";
+        String password = "pa$$word";
+        String accessToken = "token-abcd1234";
+        KiiApp app = this.getApp(APP_ID, APP_KEY);
+        Uri gatewayAddress = getGatewayAddress();
+        Context context = InstrumentationRegistry.getTargetContext();
+        GatewayAPIBuilder builder = GatewayAPIBuilder.newBuilder(context, app, gatewayAddress);
+        GatewayAPI api = builder.build();
+
+        this.addMockResponseForLogin(200, accessToken);
+        api.login(username, password);
+        Assert.assertEquals(accessToken, api.getAccessToken());
+
+        SharedPreferences preferences = context.getSharedPreferences(
+                "com.kii.thingif.preferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("GatewayAPI_VERSION", "0.0.0");
+        editor.apply();
+
+        GatewayAPI.loadFromStoredInstance(context);
+    }
+
+    @Test
+    public void loadFromStoredInstanceUpperTest() throws Exception {
+        String username = "user01";
+        String password = "pa$$word";
+        String accessToken = "token-abcd1234";
+        KiiApp app = this.getApp(APP_ID, APP_KEY);
+        Uri gatewayAddress = getGatewayAddress();
+        Context context = InstrumentationRegistry.getTargetContext();
+        GatewayAPIBuilder builder = GatewayAPIBuilder.newBuilder(context, app, gatewayAddress);
+        GatewayAPI api = builder.build();
+
+        this.addMockResponseForLogin(200, accessToken);
+        api.login(username, password);
+        Assert.assertEquals(accessToken, api.getAccessToken());
+
+        SharedPreferences preferences = context.getSharedPreferences(
+                "com.kii.thingif.preferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("GatewayAPI_VERSION", "1000.0.0");
+        editor.apply();
+
+        GatewayAPI restoredApi = GatewayAPI.loadFromStoredInstance(context);
+
+        assertEquals(api.getAppID(), restoredApi.getAppID());
+        assertEquals(api.getAppKey(), restoredApi.getAppKey());
+        assertEquals(api.getGatewayAddress().toString(), restoredApi.getGatewayAddress().toString());
+        assertEquals(api.getGatewayAddress().getPort(), restoredApi.getGatewayAddress().getPort());
+        assertEquals(api.getGatewayAddress().getScheme(), restoredApi.getGatewayAddress().getScheme());
+        Assert.assertEquals(api.getAccessToken(), restoredApi.getAccessToken());
+    }
 }
