812db6d5cea8a5468b679075a1ac8c4d0705a9af
yongping <yongping.chen@kii.com>
2017-01-12 12:30:17 +0900

    Remove actionFieldName annotation

--- thingif/src/main/java/com/kii/thingif/command/ActionAnnotation.java
+++ thingif/src/main/java/com/kii/thingif/command/ActionAnnotation.java
@@ -6,10 +6,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Define annotations to determine information of a {@link Action}
+ * @since 1.0
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface ActionAnnotation {
     String actionName() default "";
-    String actionFieldName() default "";
 }
