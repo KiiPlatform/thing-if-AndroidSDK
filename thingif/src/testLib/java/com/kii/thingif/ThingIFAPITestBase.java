6bd5257eb8cabd3568a41bd5ef1d3b3474bd3593
yongping <yongping.chen@kii.com>
2017-02-01 19:27:52 +0900

    add small test for storeInstance and loadStoredInstance for ThingIFAPI

--- thingif/src/testLib/java/com/kii/thingif/ThingIFAPITestBase.java
+++ thingif/src/testLib/java/com/kii/thingif/ThingIFAPITestBase.java
@@ -1,10 +1,16 @@
 package com.kii.thingif;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.kii.thingif.actions.AirConditionerActions;
+import com.kii.thingif.actions.HumidityActions;
+import com.kii.thingif.command.Action;
+import com.kii.thingif.states.AirConditionerState;
+import com.kii.thingif.states.HumidityState;
 import com.squareup.okhttp.mockwebserver.MockResponse;
 import com.squareup.okhttp.mockwebserver.MockWebServer;
 import com.squareup.okhttp.mockwebserver.RecordedRequest;
@@ -21,10 +27,26 @@ import java.util.regex.Pattern;
 
 public class ThingIFAPITestBase extends SmallTestBase {
 
-    protected static final String APP_ID = "smalltest";
-    private static final String SDK_VERSION = "0.13.0";
+    protected final String APP_ID = "smalltest";
+    protected final String APP_KEY = "abcdefghijklmnopqrstuvwxyz123456789";
+
+    private final String SDK_VERSION = "0.13.0";
 
     protected MockWebServer server;
+
+    protected static Map<String, Class<? extends Action>> getDefaultActionTypes () {
+        Map<String, Class<? extends Action>> actionTypes = new HashMap<>();
+        actionTypes.put("airConditionerAlias", AirConditionerActions.class);
+        actionTypes.put("humidityAlias", HumidityActions.class);
+        return actionTypes;
+    }
+
+    protected static Map<String, Class<? extends TargetState>> getDefaultStateTypes () {
+        Map<String, Class<? extends TargetState>> stateTypes = new HashMap<>();
+        stateTypes.put("airConditionerAlias", AirConditionerState.class);
+        stateTypes.put("humidityAlias", HumidityState.class);
+        return stateTypes;
+    }
 
     protected void addMockResponseForOnBoard(int httpStatus, String thingID, String accessToken) {
         MockResponse response = new MockResponse().setResponseCode(httpStatus);
@@ -41,8 +63,25 @@ public class ThingIFAPITestBase extends SmallTestBase {
         String ownerID = UUID.randomUUID().toString();
         Owner owner = new Owner(new TypedID(TypedID.Types.USER, ownerID), "owner-access-token-1234");
         KiiApp app = getApp(appID, appKey);
-        ThingIFAPI.Builder builder = ThingIFAPI.Builder.newBuilder(context, app, owner);
+        ThingIFAPI.Builder builder = ThingIFAPI.Builder.newBuilder(
+                context,
+                app,
+                owner,
+                getDefaultActionTypes(),
+                getDefaultStateTypes());
         return builder.build();
+    }
+
+    protected ThingIFAPI.Builder createDefaultThingIFAPIBuilder(Context context, String appID, String appKey) throws Exception {
+        String ownerID = UUID.randomUUID().toString();
+        Owner owner = new Owner(new TypedID(TypedID.Types.USER, ownerID), "owner-access-token-1234");
+        KiiApp app = getApp(appID, appKey);
+        return ThingIFAPI.Builder.newBuilder(
+                context,
+                app,
+                owner,
+                getDefaultActionTypes(),
+                getDefaultStateTypes());
     }
 
     public KiiApp getApp(String appId, String appKey) {
@@ -101,5 +140,12 @@ public class ThingIFAPITestBase extends SmallTestBase {
     }
     protected void addEmptyMockResponse(int httpStatus) {
         this.server.enqueue(new MockResponse().setResponseCode(httpStatus));
+    }
+
+    protected void clearSharedPreferences(Context context) throws Exception {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("com.kii.thingif.preferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.commit();
     }
 }
