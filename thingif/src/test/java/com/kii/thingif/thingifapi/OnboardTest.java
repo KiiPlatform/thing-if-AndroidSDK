b270bddd4b527550f40fc4ee8f8f8d285add35a6
yongping <yongping.chen@kii.com>
2017-02-08 18:29:57 +0900

    fix failed tests and remove redundancy tests

--- thingif/src/test/java/com/kii/thingif/thingifapi/OnboardTest.java
+++ thingif/src/test/java/com/kii/thingif/thingifapi/OnboardTest.java
@@ -49,173 +49,6 @@ public class OnboardTest extends ThingIFAPITestBase {
         this.server.shutdown();
     }
     @Test
-    public void onboardWithVendorThingIDByOwnerTest() throws Exception {
-        String vendorThingID = "v1234567890abcde";
-        String thingPassword = "password";
-        JSONObject thingProperties = new JSONObject();
-        thingProperties.put("manufacturer", "Kii");
-        String thingID = "th.1234567890";
-        String accessToken = "thing-access-token-1234";
-        this.addMockResponseForOnBoard(200, thingID, accessToken);
-        ThingIFAPI api = this.createDefaultThingIFAPI(this.context, APP_ID, APP_KEY);
-        Assert.assertFalse(api.onboarded());
-        OnboardWithVendorThingIDOptions options =
-                new OnboardWithVendorThingIDOptions
-                        .Builder()
-                        .setThingType(DEMO_THING_TYPE)
-                        .setThingProperties(thingProperties)
-                        .build();
-        Target target = api.onboard(vendorThingID, thingPassword, options);
-        Assert.assertTrue(api.onboarded());
-
-        // verify the result
-        Assert.assertEquals(new TypedID(TypedID.Types.THING, thingID), target.getTypedID());
-        Assert.assertEquals(accessToken, target.getAccessToken());
-        // verify the request
-        RecordedRequest request = this.server.takeRequest(1, TimeUnit.SECONDS);
-        Assert.assertEquals(BASE_PATH + "/onboardings", request.getPath());
-        Assert.assertEquals("POST", request.getMethod());
-
-        Map<String, String> expectedRequestHeaders = new HashMap<String, String>();
-        expectedRequestHeaders.put("X-Kii-AppID", APP_ID);
-        expectedRequestHeaders.put("X-Kii-AppKey", APP_KEY);
-        expectedRequestHeaders.put("Authorization", "Bearer " + api.getOwner().getAccessToken());
-        expectedRequestHeaders.put("Content-Type", "application/vnd.kii.OnboardingWithVendorThingIDByOwner+json");
-        this.assertRequestHeader(expectedRequestHeaders, request);
-
-        JsonObject expectedRequestBody = new JsonObject();
-        expectedRequestBody.addProperty("owner", api.getOwner().getTypedID().toString());
-        expectedRequestBody.addProperty("vendorThingID", vendorThingID);
-        expectedRequestBody.addProperty("thingPassword", thingPassword);
-        expectedRequestBody.addProperty("thingType", DEMO_THING_TYPE);
-        expectedRequestBody.add("thingProperties", new JsonParser().parse(thingProperties.toString()));
-        this.assertRequestBody(expectedRequestBody, request);
-    }
-    @Test
-    public void onboardWithVendorThingIDByOwner403ErrorTest() throws Exception {
-        String vendorThingID = "v1234567890abcde";
-        String thingPassword = "password";
-        JSONObject thingProperties = new JSONObject();
-        thingProperties.put("manufacturer", "Kii");
-        this.addEmptyMockResponse(403);
-
-        ThingIFAPI api = this.createDefaultThingIFAPI(this.context, APP_ID, APP_KEY);
-        try {
-            OnboardWithVendorThingIDOptions options =
-                    new OnboardWithVendorThingIDOptions
-                            .Builder()
-                            .setThingType(DEMO_THING_TYPE)
-                            .setThingProperties(thingProperties)
-                            .build();
-
-            api.onboard(vendorThingID, thingPassword, options);
-            Assert.fail("ThingIFRestException should be thrown");
-        } catch (ForbiddenException e) {
-        }
-        // verify the request
-        RecordedRequest request = this.server.takeRequest(1, TimeUnit.SECONDS);
-        Assert.assertEquals(BASE_PATH + "/onboardings", request.getPath());
-        Assert.assertEquals("POST", request.getMethod());
-
-        Map<String, String> expectedRequestHeaders = new HashMap<String, String>();
-        expectedRequestHeaders.put("X-Kii-AppID", APP_ID);
-        expectedRequestHeaders.put("X-Kii-AppKey", APP_KEY);
-        expectedRequestHeaders.put("Authorization", "Bearer " + api.getOwner().getAccessToken());
-        expectedRequestHeaders.put("Content-Type", "application/vnd.kii.OnboardingWithVendorThingIDByOwner+json");
-        this.assertRequestHeader(expectedRequestHeaders, request);
-
-        JsonObject expectedRequestBody = new JsonObject();
-        expectedRequestBody.addProperty("owner", api.getOwner().getTypedID().toString());
-        expectedRequestBody.addProperty("vendorThingID", vendorThingID);
-        expectedRequestBody.addProperty("thingPassword", thingPassword);
-        expectedRequestBody.addProperty("thingType", DEMO_THING_TYPE);
-        expectedRequestBody.add("thingProperties", new JsonParser().parse(thingProperties.toString()));
-        this.assertRequestBody(expectedRequestBody, request);
-    }
-
-    @Test
-    public void onboardWithVendorThingIDByOwner404ErrorTest() throws Exception {
-        String vendorThingID = "v1234567890abcde";
-        String thingPassword = "password";
-        JSONObject thingProperties = new JSONObject();
-        thingProperties.put("manufacturer", "Kii");
-        this.addEmptyMockResponse(404);
-
-        ThingIFAPI api = this.createDefaultThingIFAPI(this.context, APP_ID, APP_KEY);
-        try {
-            OnboardWithVendorThingIDOptions options =
-                    new OnboardWithVendorThingIDOptions
-                            .Builder()
-                            .setThingType(DEMO_THING_TYPE)
-                            .setThingProperties(thingProperties)
-                            .build();
-
-            api.onboard(vendorThingID, thingPassword, options);
-            Assert.fail("ThingIFRestException should be thrown");
-        } catch (NotFoundException e) {
-        }
-        // verify the request
-        RecordedRequest request = this.server.takeRequest(1, TimeUnit.SECONDS);
-        Assert.assertEquals(BASE_PATH + "/onboardings", request.getPath());
-        Assert.assertEquals("POST", request.getMethod());
-
-        Map<String, String> expectedRequestHeaders = new HashMap<String, String>();
-        expectedRequestHeaders.put("X-Kii-AppID", APP_ID);
-        expectedRequestHeaders.put("X-Kii-AppKey", APP_KEY);
-        expectedRequestHeaders.put("Authorization", "Bearer " + api.getOwner().getAccessToken());
-        expectedRequestHeaders.put("Content-Type", "application/vnd.kii.OnboardingWithVendorThingIDByOwner+json");
-        this.assertRequestHeader(expectedRequestHeaders, request);
-
-        JsonObject expectedRequestBody = new JsonObject();
-        expectedRequestBody.addProperty("owner", api.getOwner().getTypedID().toString());
-        expectedRequestBody.addProperty("vendorThingID", vendorThingID);
-        expectedRequestBody.addProperty("thingPassword", thingPassword);
-        expectedRequestBody.addProperty("thingType", DEMO_THING_TYPE);
-        expectedRequestBody.add("thingProperties", new JsonParser().parse(thingProperties.toString()));
-        this.assertRequestBody(expectedRequestBody, request);
-    }
-    @Test
-    public void onboardWithVendorThingIDByOwner500ErrorTest() throws Exception {
-        String vendorThingID = "v1234567890abcde";
-        String thingPassword = "password";
-        JSONObject thingProperties = new JSONObject();
-        thingProperties.put("manufacturer", "Kii");
-        this.addEmptyMockResponse(500);
-
-        ThingIFAPI api = this.createDefaultThingIFAPI(this.context, APP_ID, APP_KEY);
-        try {
-            OnboardWithVendorThingIDOptions options =
-                    new OnboardWithVendorThingIDOptions
-                            .Builder()
-                            .setThingType(DEMO_THING_TYPE)
-                            .setThingProperties(thingProperties)
-                            .build();
-
-            api.onboard(vendorThingID, thingPassword, options);
-            Assert.fail("ThingIFRestException should be thrown");
-        } catch (InternalServerErrorException e) {
-        }
-        // verify the request
-        RecordedRequest request = this.server.takeRequest(1, TimeUnit.SECONDS);
-        Assert.assertEquals(BASE_PATH + "/onboardings", request.getPath());
-        Assert.assertEquals("POST", request.getMethod());
-
-        Map<String, String> expectedRequestHeaders = new HashMap<String, String>();
-        expectedRequestHeaders.put("X-Kii-AppID", APP_ID);
-        expectedRequestHeaders.put("X-Kii-AppKey", APP_KEY);
-        expectedRequestHeaders.put("Authorization", "Bearer " + api.getOwner().getAccessToken());
-        expectedRequestHeaders.put("Content-Type", "application/vnd.kii.OnboardingWithVendorThingIDByOwner+json");
-        this.assertRequestHeader(expectedRequestHeaders, request);
-
-        JsonObject expectedRequestBody = new JsonObject();
-        expectedRequestBody.addProperty("owner", api.getOwner().getTypedID().toString());
-        expectedRequestBody.addProperty("vendorThingID", vendorThingID);
-        expectedRequestBody.addProperty("thingPassword", thingPassword);
-        expectedRequestBody.addProperty("thingType", DEMO_THING_TYPE);
-        expectedRequestBody.add("thingProperties", new JsonParser().parse(thingProperties.toString()));
-        this.assertRequestBody(expectedRequestBody, request);
-    }
-    @Test
     public void onboardTwiceTest() throws Exception {
         String vendorThingID = "v1234567890abcde";
         String thingPassword = "password";
@@ -244,144 +77,6 @@ public class OnboardTest extends ThingIFAPITestBase {
         } catch (IllegalStateException e) {
         }
     }
-    @Test(expected = IllegalArgumentException.class)
-    public void onboardWithVendorThingIDByOwnerWithNullVendorThingIDTest() throws Exception {
-        ThingIFAPI api = this.createDefaultThingIFAPI(this.context,APP_ID, APP_KEY);
-        OnboardWithVendorThingIDOptions options =
-                new OnboardWithVendorThingIDOptions
-                        .Builder()
-                        .setThingType(DEMO_THING_TYPE)
-                        .build();
-
-        Target target = api.onboard(null, "password", options);
-    }
-    @Test(expected = IllegalArgumentException.class)
-    public void onboardWithVendorThingIDByOwnerWithEmptyVendorThingIDTest() throws Exception {
-        ThingIFAPI api = this.createDefaultThingIFAPI(this.context,APP_ID, APP_KEY);
-        OnboardWithVendorThingIDOptions options =
-                new OnboardWithVendorThingIDOptions
-                        .Builder()
-                        .setThingType(DEMO_THING_TYPE)
-                        .build();
-
-        Target target = api.onboard("", "password", options);
-    }
-    @Test(expected = IllegalArgumentException.class)
-    public void onboardWithVendorThingIDByOwnerWithNullVendorThingPasswordTest() throws Exception {
-        ThingIFAPI api = this.createDefaultThingIFAPI(this.context,APP_ID, APP_KEY);
-        OnboardWithVendorThingIDOptions options =
-                new OnboardWithVendorThingIDOptions
-                        .Builder()
-                        .setThingType(DEMO_THING_TYPE)
-                        .build();
-        Target target = api.onboard("v1234567890abcde", null, options);
-    }
-    @Test(expected = IllegalArgumentException.class)
-    public void onboardWithVendorThingIDByOwnerWithEmptyVendorThingPasswordTest() throws Exception {
-        ThingIFAPI api = this.createDefaultThingIFAPI(this.context,APP_ID, APP_KEY);
-        OnboardWithVendorThingIDOptions options =
-                new OnboardWithVendorThingIDOptions
-                        .Builder()
-                        .setThingType(DEMO_THING_TYPE)
-                        .build();
-        Target target = api.onboard("v1234567890abcde", "", options);
-    }
-    @Test
-    public void onboardWithThingIDByOwnerTest() throws Exception {
-        String thingID = "th.1234567890";
-        String thingPassword = "password";
-        String accessToken = "thing-access-token-1234";
-        this.addMockResponseForOnBoard(200, thingID, accessToken);
-
-        ThingIFAPI api = this.createDefaultThingIFAPI(this.context,APP_ID, APP_KEY);
-        Assert.assertFalse(api.onboarded());
-        Target target = api.onboard(thingID, thingPassword, (OnboardWithThingIDOptions) null);
-        Assert.assertTrue(api.onboarded());
-
-        // verify the result
-        Assert.assertEquals(new TypedID(TypedID.Types.THING, thingID), target.getTypedID());
-        Assert.assertEquals(accessToken, target.getAccessToken());
-        // verify the request
-        RecordedRequest request = this.server.takeRequest(1, TimeUnit.SECONDS);
-        Assert.assertEquals(BASE_PATH + "/onboardings", request.getPath());
-        Assert.assertEquals("POST", request.getMethod());
-
-        Map<String, String> expectedRequestHeaders = new HashMap<String, String>();
-        expectedRequestHeaders.put("X-Kii-AppID", APP_ID);
-        expectedRequestHeaders.put("X-Kii-AppKey", APP_KEY);
-        expectedRequestHeaders.put("Authorization", "Bearer " + api.getOwner().getAccessToken());
-        expectedRequestHeaders.put("Content-Type", "application/vnd.kii.OnboardingWithThingIDByOwner+json");
-        this.assertRequestHeader(expectedRequestHeaders, request);
-
-        JsonObject expectedRequestBody = new JsonObject();
-        expectedRequestBody.addProperty("owner", api.getOwner().getTypedID().toString());
-        expectedRequestBody.addProperty("thingID", thingID);
-        expectedRequestBody.addProperty("thingPassword", thingPassword);
-        this.assertRequestBody(expectedRequestBody, request);
-    }
-    @Test
-    public void onboardWithThingIDByOwner403ErrorTest() throws Exception {
-        String thingID = "th.1234567890";
-        String thingPassword = "password";
-        String accessToken = "thing-access-token-1234";
-        this.addMockResponseForOnBoard(403, null, null);
-
-        ThingIFAPI api = this.createDefaultThingIFAPI(this.context,APP_ID, APP_KEY);
-        try {
-            api.onboard(thingID, thingPassword, (OnboardWithThingIDOptions)null);
-            Assert.fail("ThingIFRestException should be thrown");
-        } catch (ForbiddenException e) {
-        }
-    }
-    @Test
-    public void onboardWithThingIDByOwner404ErrorTest() throws Exception {
-        String thingID = "th.1234567890";
-        String thingPassword = "password";
-        String accessToken = "thing-access-token-1234";
-        this.addMockResponseForOnBoard(404, null, null);
-
-        ThingIFAPI api = this.createDefaultThingIFAPI(this.context,APP_ID, APP_KEY);
-        try {
-            api.onboard(thingID, thingPassword, (OnboardWithThingIDOptions)null);
-            Assert.fail("ThingIFRestException should be thrown");
-        } catch (NotFoundException e) {
-        }
-    }
-    @Test
-    public void onboardWithThingIDByOwner500ErrorTest() throws Exception {
-        String thingID = "th.1234567890";
-        String thingPassword = "password";
-        String accessToken = "thing-access-token-1234";
-        this.addMockResponseForOnBoard(500, null, null);
-
-        ThingIFAPI api = this.createDefaultThingIFAPI(this.context,APP_ID, APP_KEY);
-        try {
-            api.onboard(thingID, thingPassword, (OnboardWithThingIDOptions)null);
-            Assert.fail("ThingIFRestException should be thrown");
-        } catch (InternalServerErrorException e) {
-        }
-    }
-    @Test(expected = IllegalArgumentException.class)
-    public void onboardWithThingIDByOwnerTestWithNullThingIDTest() throws Exception {
-        ThingIFAPI api = this.createDefaultThingIFAPI(this.context,APP_ID, APP_KEY);
-        Target target = api.onboard(null, "password", (OnboardWithThingIDOptions)null);
-    }
-    @Test(expected = IllegalArgumentException.class)
-    public void onboardWithThingIDByOwnerTestWithEmptyThingIDTest() throws Exception {
-        ThingIFAPI api = this.createDefaultThingIFAPI(this.context,APP_ID, APP_KEY);
-        Target target = api.onboard("", "password", (OnboardWithThingIDOptions)null);
-    }
-    @Test(expected = IllegalArgumentException.class)
-    public void onboardWithThingIDByOwnerTestWithNullPasswordTest() throws Exception {
-        ThingIFAPI api = this.createDefaultThingIFAPI(this.context,APP_ID, APP_KEY);
-        Target target = api.onboard("th.1234567890", null, (OnboardWithThingIDOptions)null);
-    }
-    @Test(expected = IllegalArgumentException.class)
-    public void onboardWithThingIDByOwnerTestWithEmptyPasswordTest() throws Exception {
-        ThingIFAPI api = this.createDefaultThingIFAPI(this.context, APP_ID, APP_KEY);
-        Target target = api.onboard("th.1234567890", "", (OnboardWithThingIDOptions)null);
-    }
-
     @Test
     public void onboardWithVendorThingIDAndOptionsTest() throws Exception {
         String vendorThingID = "v1234567890abcde";
@@ -425,7 +120,6 @@ public class OnboardTest extends ThingIFAPITestBase {
         expectedRequestBody.addProperty("firmwareVersion", "dummyVersion");
         expectedRequestBody.add("thingProperties", new JsonParser().parse(thingProperties.toString()));
         expectedRequestBody.addProperty("layoutPosition", "STANDALONE");
-        expectedRequestBody.addProperty("dataGroupingInterval", "1_MINUTE");
         this.assertRequestBody(expectedRequestBody, request);
     }
     @Test
@@ -465,7 +159,6 @@ public class OnboardTest extends ThingIFAPITestBase {
         expectedRequestBody.addProperty("thingType", DEMO_THING_TYPE);
         expectedRequestBody.add("thingProperties", new JsonParser().parse(thingProperties.toString()));
         expectedRequestBody.addProperty("layoutPosition", "GATEWAY");
-        expectedRequestBody.addProperty("dataGroupingInterval", "15_MINUTES");
         this.assertRequestBody(expectedRequestBody, request);
     }
     @Test
@@ -505,7 +198,6 @@ public class OnboardTest extends ThingIFAPITestBase {
         expectedRequestBody.addProperty("thingType", DEMO_THING_TYPE);
         expectedRequestBody.add("thingProperties", new JsonParser().parse(thingProperties.toString()));
         expectedRequestBody.addProperty("layoutPosition", "ENDNODE");
-        expectedRequestBody.addProperty("dataGroupingInterval", "30_MINUTES");
         this.assertRequestBody(expectedRequestBody, request);
     }
     @Test
@@ -545,7 +237,6 @@ public class OnboardTest extends ThingIFAPITestBase {
         expectedRequestBody.addProperty("thingType", DEMO_THING_TYPE);
         expectedRequestBody.add("thingProperties", new JsonParser().parse(thingProperties.toString()));
         expectedRequestBody.addProperty("layoutPosition", "STANDALONE");
-        expectedRequestBody.addProperty("dataGroupingInterval", "1_HOUR");
         this.assertRequestBody(expectedRequestBody, request);
     }
     @Test
@@ -623,7 +314,6 @@ public class OnboardTest extends ThingIFAPITestBase {
 
         JsonObject expectedRequestBody = new JsonObject();
         expectedRequestBody.addProperty("owner", api.getOwner().getTypedID().toString());
-        expectedRequestBody.addProperty("dataGroupingInterval", "12_HOURS");
         expectedRequestBody.addProperty("thingID", thingID);
         expectedRequestBody.addProperty("layoutPosition", "STANDALONE");
         expectedRequestBody.addProperty("thingPassword", thingPassword);
