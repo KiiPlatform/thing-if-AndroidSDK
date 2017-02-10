6fc531c7897bead0abde7a55079c066d92d13605
yongping <yongping.chen@kii.com>
2017-02-10 14:07:27 +0900

    Revert "Revert "Implement AliasActionAdapter and add tests""
    This reverts commit 61b10a669cfd85b23bf950f37b46dda467f1853b.

---
+++ thingif/src/main/java/com/kii/thingif/internal/gson/AliasActionAdapter.java
@@ -0,0 +1,54 @@
+package com.kii.thingif.internal.gson;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.kii.thingif.command.Action;
+import com.kii.thingif.command.AliasAction;
+
+import java.lang.reflect.Type;
+import java.util.Map;
+
+public class AliasActionAdapter implements
+        JsonSerializer<AliasAction>,
+        JsonDeserializer<AliasAction> {
+
+    private Map<String, Class<? extends Action>> actionTypes;
+
+    public AliasActionAdapter(Map<String, Class<? extends Action>> actionTypes) {
+        this.actionTypes = actionTypes;
+    }
+    @Override
+    public JsonElement serialize(AliasAction src, Type typeOfSrc, JsonSerializationContext context) {
+        if (src == null) return null;
+
+        JsonObject json = new JsonObject();
+        json.add(
+                src.getAlias(),
+                new Gson().toJsonTree(src.getAction()));
+        return json;
+    }
+
+    @Override
+    public AliasAction deserialize(JsonElement jsonElement, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        if (jsonElement == null) return null;
+
+        JsonObject json = jsonElement.getAsJsonObject();
+
+        for (Map.Entry<String, Class<? extends Action>> entry: this.actionTypes.entrySet()) {
+            String alias = entry.getKey();
+            Class<? extends Action> actionClass = entry.getValue();
+            if (json.has(alias)) {
+                JsonObject actionJson = json.getAsJsonObject(alias);
+                Action action = new Gson().fromJson(actionJson, this.actionTypes.get(alias));
+                return new AliasAction(alias, action);
+            }
+        }
+        return null;
+    }
+}
