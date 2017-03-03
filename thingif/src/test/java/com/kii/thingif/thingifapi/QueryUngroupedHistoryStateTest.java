30b53c8b1a19b370e05889f90bd79791e861ac37
yongping <yongping.chen@kii.com>
2017-03-03 12:40:52 +0900

    Add more small tests

--- thingif/src/test/java/com/kii/thingif/thingifapi/QueryUngroupedHistoryStateTest.java
+++ thingif/src/test/java/com/kii/thingif/thingifapi/QueryUngroupedHistoryStateTest.java
@@ -9,11 +9,15 @@ import com.kii.thingif.TargetState;
 import com.kii.thingif.ThingIFAPI;
 import com.kii.thingif.ThingIFAPITestBase;
 import com.kii.thingif.clause.query.AllClause;
+import com.kii.thingif.exception.BadRequestException;
+import com.kii.thingif.exception.ConflictException;
+import com.kii.thingif.exception.ForbiddenException;
+import com.kii.thingif.exception.NotFoundException;
+import com.kii.thingif.exception.ServiceUnavailableException;
 import com.kii.thingif.exception.UnregisteredAliasException;
 import com.kii.thingif.query.HistoryState;
 import com.kii.thingif.query.HistoryStatesQuery;
 import com.kii.thingif.states.AirConditionerState;
-import com.kii.thingif.states.HumidityState;
 import com.kii.thingif.thingifapi.utils.ThingIFAPIUtils;
 import com.kii.thingif.utils.JsonUtil;
 import com.squareup.okhttp.mockwebserver.MockWebServer;
@@ -39,17 +43,31 @@ import java.util.concurrent.TimeUnit;
 @RunWith(RobolectricTestRunner.class)
 public class QueryUngroupedHistoryStateTest extends ThingIFAPITestBase {
     private Context context;
+    private ThingIFAPI defaultApi;
 
     @Before
     public void before() throws Exception {
         context = RuntimeEnvironment.application.getApplicationContext();
         this.server = new MockWebServer();
         this.server.start();
+
+        Target target = new StandaloneThing("thing1", "vendor-thing-1", "dummyToken");
+        this.defaultApi = this.createDefaultThingIFAPI(this.context, APP_ID, APP_KEY);
+        ThingIFAPIUtils.setTarget(defaultApi, target);
     }
 
     @After
     public void after() throws Exception {
         this.server.shutdown();
+    }
+
+    private HistoryStatesQuery getDefaultQuery() {
+        return HistoryStatesQuery.Builder
+                .newBuilder(ALIAS1, new AllClause())
+                .setBestEffortLimit(5)
+                .setNextPaginationKey("100/2")
+                .setFirmwareVersion("v1")
+                .build();
     }
 
     @Test
@@ -114,6 +132,51 @@ public class QueryUngroupedHistoryStateTest extends ThingIFAPITestBase {
         this.assertRequestBody(expectedRequestBody, request1);
     }
 
+    @Test
+    public void query_ungrouped_historyStates_emptyResults_response_Test() throws Exception{
+        Target target = this.defaultApi.getTarget();
+        Assert.assertNotNull(target);
+        HistoryStatesQuery query = getDefaultQuery();
+
+        List<HistoryState<? extends TargetState>> expectedStates = new ArrayList<>();
+
+        addMockResponseForQueryUngroupedHistoryState(200, expectedStates, null);
+
+        Pair<List<HistoryState<AirConditionerState>>, String> result =
+                this.defaultApi.query(query);
+
+        // verify result
+        Assert.assertNull(result.second);
+        Assert.assertEquals(0, result.first.size());
+
+        // verify request
+        RecordedRequest request1 = this.server.takeRequest(1, TimeUnit.SECONDS);
+        junit.framework.Assert.assertEquals(
+                MessageFormat.format("{0}/targets/{1}/states/aliases/{2}/query",
+                        BASE_PATH,
+                        target.getTypedID().toString(),
+                        ALIAS1),
+                request1.getPath());
+        junit.framework.Assert.assertEquals("POST", request1.getMethod());
+
+        Map<String, String> expectedRequestHeaders1 = new HashMap<>();
+        expectedRequestHeaders1.put("X-Kii-AppID", APP_ID);
+        expectedRequestHeaders1.put("X-Kii-AppKey", APP_KEY);
+        expectedRequestHeaders1.put("Authorization", "Bearer " + this.defaultApi.getOwner().getAccessToken());
+        expectedRequestHeaders1.put("Content-Type", "application/vnd.kii.TraitStateQueryRequest+json");
+        this.assertRequestHeader(expectedRequestHeaders1, request1);
+
+        JSONObject expectedRequestBody = new JSONObject();
+        expectedRequestBody.put(
+                "query",
+                new JSONObject()
+                        .put("clause", JsonUtil.queryClauseToJson(new AllClause())));
+        expectedRequestBody.putOpt("firmwareVersion", query.getFirmwareVersion());
+        expectedRequestBody.put("paginationKey", query.getNextPaginationKey());
+        expectedRequestBody.put("bestEffortLimit", query.getBestEffortLimit());
+        this.assertRequestBody(expectedRequestBody, request1);
+    }
+
     @Test(expected = UnregisteredAliasException.class)
     public void query_with_unregisteredAlias_UnregisterAliasException_should_thrown_Test() throws Exception{
         Target target = new StandaloneThing("thing1", "vendor-thing-1", "dummyToken");
@@ -147,5 +210,203 @@ public class QueryUngroupedHistoryStateTest extends ThingIFAPITestBase {
                 .setFirmwareVersion("v1")
                 .build();
         api.query(query);
+    }
+
+    @Test
+    public void query_ungrouped_historyStates_400ErrorTest() throws Exception {
+        HistoryStatesQuery query = getDefaultQuery();
+        Target target = this.defaultApi.getTarget();
+        Assert.assertNotNull(target);
+
+        this.addEmptyMockResponse(400);
+        try {
+            this.defaultApi.query(query);
+            Assert.fail("ThingIFRestException should be thrown");
+        } catch (BadRequestException e) {
+        }
+        // verify the request
+        RecordedRequest request1 = this.server.takeRequest(1, TimeUnit.SECONDS);
+        junit.framework.Assert.assertEquals(
+                MessageFormat.format("{0}/targets/{1}/states/aliases/{2}/query",
+                        BASE_PATH,
+                        target.getTypedID().toString(),
+                        ALIAS1),
+                request1.getPath());
+        Assert.assertEquals("POST", request1.getMethod());
+
+        Map<String, String> expectedRequestHeaders = new HashMap<>();
+        expectedRequestHeaders.put("X-Kii-AppID", APP_ID);
+        expectedRequestHeaders.put("X-Kii-AppKey", APP_KEY);
+        expectedRequestHeaders.put("Authorization", "Bearer " + this.defaultApi.getOwner().getAccessToken());
+        expectedRequestHeaders.put("Content-Type", "application/vnd.kii.TraitStateQueryRequest+json");
+        this.assertRequestHeader(expectedRequestHeaders, request1);
+
+        JSONObject expectedRequestBody = new JSONObject();
+        expectedRequestBody.put(
+                "query",
+                new JSONObject()
+                        .put("clause", JsonUtil.queryClauseToJson(new AllClause())));
+        expectedRequestBody.putOpt("firmwareVersion", query.getFirmwareVersion());
+        expectedRequestBody.putOpt("paginationKey", query.getNextPaginationKey());
+        expectedRequestBody.putOpt("bestEffortLimit", query.getBestEffortLimit());
+        this.assertRequestBody(expectedRequestBody, request1);
+    }
+    @Test
+    public void query_ungrouped_historyStates_403ErrorTest() throws Exception {
+        HistoryStatesQuery query = getDefaultQuery();
+        Target target = this.defaultApi.getTarget();
+        Assert.assertNotNull(target);
+
+        this.addEmptyMockResponse(403);
+        try {
+            this.defaultApi.query(query);
+            Assert.fail("ThingIFRestException should be thrown");
+        } catch (ForbiddenException e) {
+        }
+        // verify the request
+        RecordedRequest request1 = this.server.takeRequest(1, TimeUnit.SECONDS);
+        junit.framework.Assert.assertEquals(
+                MessageFormat.format("{0}/targets/{1}/states/aliases/{2}/query",
+                        BASE_PATH,
+                        target.getTypedID().toString(),
+                        ALIAS1),
+                request1.getPath());
+        Assert.assertEquals("POST", request1.getMethod());
+
+        Map<String, String> expectedRequestHeaders = new HashMap<>();
+        expectedRequestHeaders.put("X-Kii-AppID", APP_ID);
+        expectedRequestHeaders.put("X-Kii-AppKey", APP_KEY);
+        expectedRequestHeaders.put("Authorization", "Bearer " + this.defaultApi.getOwner().getAccessToken());
+        expectedRequestHeaders.put("Content-Type", "application/vnd.kii.TraitStateQueryRequest+json");
+        this.assertRequestHeader(expectedRequestHeaders, request1);
+
+        JSONObject expectedRequestBody = new JSONObject();
+        expectedRequestBody.put(
+                "query",
+                new JSONObject()
+                        .put("clause", JsonUtil.queryClauseToJson(new AllClause())));
+        expectedRequestBody.putOpt("firmwareVersion", query.getFirmwareVersion());
+        expectedRequestBody.putOpt("paginationKey", query.getNextPaginationKey());
+        expectedRequestBody.putOpt("bestEffortLimit", query.getBestEffortLimit());
+        this.assertRequestBody(expectedRequestBody, request1);
+    }
+    @Test
+    public void query_ungrouped_historyStates_404ErrorTest() throws Exception {
+        HistoryStatesQuery query = getDefaultQuery();
+        Target target = this.defaultApi.getTarget();
+        Assert.assertNotNull(target);
+
+        this.addEmptyMockResponse(404);
+        try {
+            this.defaultApi.query(query);
+            Assert.fail("ThingIFRestException should be thrown");
+        } catch (NotFoundException e) {
+        }
+        // verify the request
+        RecordedRequest request1 = this.server.takeRequest(1, TimeUnit.SECONDS);
+        junit.framework.Assert.assertEquals(
+                MessageFormat.format("{0}/targets/{1}/states/aliases/{2}/query",
+                        BASE_PATH,
+                        target.getTypedID().toString(),
+                        ALIAS1),
+                request1.getPath());
+        Assert.assertEquals("POST", request1.getMethod());
+
+        Map<String, String> expectedRequestHeaders = new HashMap<>();
+        expectedRequestHeaders.put("X-Kii-AppID", APP_ID);
+        expectedRequestHeaders.put("X-Kii-AppKey", APP_KEY);
+        expectedRequestHeaders.put("Authorization", "Bearer " + this.defaultApi.getOwner().getAccessToken());
+        expectedRequestHeaders.put("Content-Type", "application/vnd.kii.TraitStateQueryRequest+json");
+        this.assertRequestHeader(expectedRequestHeaders, request1);
+
+        JSONObject expectedRequestBody = new JSONObject();
+        expectedRequestBody.put(
+                "query",
+                new JSONObject()
+                        .put("clause", JsonUtil.queryClauseToJson(new AllClause())));
+        expectedRequestBody.putOpt("firmwareVersion", query.getFirmwareVersion());
+        expectedRequestBody.putOpt("paginationKey", query.getNextPaginationKey());
+        expectedRequestBody.putOpt("bestEffortLimit", query.getBestEffortLimit());
+        this.assertRequestBody(expectedRequestBody, request1);
+    }
+    @Test
+    public void query_ungrouped_historyStates_503ErrorTest() throws Exception {
+
+        HistoryStatesQuery query = getDefaultQuery();
+        Target target = this.defaultApi.getTarget();
+        Assert.assertNotNull(target);
+
+        this.addEmptyMockResponse(503);
+        try {
+            this.defaultApi.query(query);
+            Assert.fail("ThingIFRestException should be thrown");
+        } catch (ServiceUnavailableException e) {
+        }
+        // verify the request
+        RecordedRequest request1 = this.server.takeRequest(1, TimeUnit.SECONDS);
+        junit.framework.Assert.assertEquals(
+                MessageFormat.format("{0}/targets/{1}/states/aliases/{2}/query",
+                        BASE_PATH,
+                        target.getTypedID().toString(),
+                        ALIAS1),
+                request1.getPath());
+        Assert.assertEquals("POST", request1.getMethod());
+
+        Map<String, String> expectedRequestHeaders = new HashMap<>();
+        expectedRequestHeaders.put("X-Kii-AppID", APP_ID);
+        expectedRequestHeaders.put("X-Kii-AppKey", APP_KEY);
+        expectedRequestHeaders.put("Authorization", "Bearer " + this.defaultApi.getOwner().getAccessToken());
+        expectedRequestHeaders.put("Content-Type", "application/vnd.kii.TraitStateQueryRequest+json");
+        this.assertRequestHeader(expectedRequestHeaders, request1);
+
+        JSONObject expectedRequestBody = new JSONObject();
+        expectedRequestBody.put(
+                "query",
+                new JSONObject()
+                        .put("clause", JsonUtil.queryClauseToJson(new AllClause())));
+        expectedRequestBody.putOpt("firmwareVersion", query.getFirmwareVersion());
+        expectedRequestBody.putOpt("paginationKey", query.getNextPaginationKey());
+        expectedRequestBody.putOpt("bestEffortLimit", query.getBestEffortLimit());
+        this.assertRequestBody(expectedRequestBody, request1);
+    }
+
+    @Test
+    public void query_ungrouped_historyStates_409ErrorTest() throws Exception {
+        HistoryStatesQuery query = getDefaultQuery();
+        Target target = this.defaultApi.getTarget();
+        Assert.assertNotNull(target);
+
+        this.addEmptyMockResponse(409);
+        try {
+            this.defaultApi.query(query);
+            Assert.fail("ThingIFRestException should be thrown");
+        } catch (ConflictException e) {
+        }
+        // verify the request
+        RecordedRequest request1 = this.server.takeRequest(1, TimeUnit.SECONDS);
+        junit.framework.Assert.assertEquals(
+                MessageFormat.format("{0}/targets/{1}/states/aliases/{2}/query",
+                        BASE_PATH,
+                        target.getTypedID().toString(),
+                        ALIAS1),
+                request1.getPath());
+        Assert.assertEquals("POST", request1.getMethod());
+
+        Map<String, String> expectedRequestHeaders = new HashMap<>();
+        expectedRequestHeaders.put("X-Kii-AppID", APP_ID);
+        expectedRequestHeaders.put("X-Kii-AppKey", APP_KEY);
+        expectedRequestHeaders.put("Authorization", "Bearer " + this.defaultApi.getOwner().getAccessToken());
+        expectedRequestHeaders.put("Content-Type", "application/vnd.kii.TraitStateQueryRequest+json");
+        this.assertRequestHeader(expectedRequestHeaders, request1);
+
+        JSONObject expectedRequestBody = new JSONObject();
+        expectedRequestBody.put(
+                "query",
+                new JSONObject()
+                        .put("clause", JsonUtil.queryClauseToJson(new AllClause())));
+        expectedRequestBody.putOpt("firmwareVersion", query.getFirmwareVersion());
+        expectedRequestBody.putOpt("paginationKey", query.getNextPaginationKey());
+        expectedRequestBody.putOpt("bestEffortLimit", query.getBestEffortLimit());
+        this.assertRequestBody(expectedRequestBody, request1);
     }
 }
