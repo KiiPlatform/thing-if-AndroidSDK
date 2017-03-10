1f3eba926171ce61d8b5a7290c83d465cad937c7
yongping <yongping.chen@kii.com>
2017-03-10 19:41:38 +0900

    implement parcelable for GatewayInformation

---
+++ thingif/src/test/java/com/kii/thingif/gateway/GatewayInformationTest.java
@@ -0,0 +1,23 @@
+package com.kii.thingif.gateway;
+
+import android.os.Parcel;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class GatewayInformationTest {
+    @Test
+    public void parcelableTest() {
+        GatewayInformation expected = new GatewayInformation("vendor-id");
+
+        Parcel parcel = Parcel.obtain();
+        expected.writeToParcel(parcel, 0);
+        parcel.setDataPosition(0);
+        GatewayInformation deserialized = GatewayInformation.CREATOR.createFromParcel(parcel);
+
+        Assert.assertEquals(expected.getVendorThingID(), deserialized.getVendorThingID());
+    }
+}
