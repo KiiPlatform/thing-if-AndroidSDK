39e25a719ceff8cda767cb898b0b3f6afba55c9c
naoki.akimoto <naoki.akimoto@muraoka-design.com>
2017-04-05 14:59:58 +0900

    add large test for ThingIFAPI.getTargetState.

---
+++ thingiftest/src/androidTest/java/com/kii/thingif/largetests/GetTargetStateTest.java
@@ -0,0 +1,81 @@
+package com.kii.thingif.largetests;
+
+import com.kii.thingif.OnboardWithVendorThingIDOptions;
+import com.kii.thingif.Target;
+import com.kii.thingif.TargetState;
+import com.kii.thingif.ThingIFAPI;
+import com.kii.thingif.TypedID;
+import com.kii.thingif.exception.NotFoundException;
+import com.kii.thingif.states.AirConditionerState;
+import com.kii.thingif.states.HumidityState;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class GetTargetStateTest extends LargeTestCaseBase {
+
+    private ThingIFAPI onboardedApi;
+
+    @Before
+    public void before() throws Exception{
+        super.before();
+        this.onboardedApi = this.createDefaultThingIFAPI();
+        String vendorThingID = UUID.randomUUID().toString();
+        String thingPassword = "password";
+        // on-boarding thing
+        OnboardWithVendorThingIDOptions options =
+                new OnboardWithVendorThingIDOptions.Builder()
+                        .setThingType(DEFAULT_THING_TYPE)
+                        .setFirmwareVersion(DEFAULT_FIRMWARE_VERSION).build();
+        Target target = this.onboardedApi.onboardWithVendorThingID(vendorThingID, thingPassword, options);
+        Assert.assertEquals(TypedID.Types.THING, target.getTypedID().getType());
+        Assert.assertNotNull(target.getAccessToken());
+    }
+
+    @Test
+    public void successEmptyResultsTest() throws Exception {
+        Map results = onboardedApi.getTargetState();
+
+        Assert.assertNotNull(results);
+        Assert.assertEquals(0, results.size());
+
+        try {
+            onboardedApi.getTargetState(ALIAS1, AirConditionerState.class);
+            Assert.fail("should fail");
+        } catch (NotFoundException e) {
+            Assert.assertEquals("STATE_NOT_FOUND", e.getErrorCode());
+            Assert.assertNotNull(e.getMessage());
+            Assert.assertEquals(404, e.getStatusCode());
+        }
+    }
+
+    @Test
+    public void successTest() throws Exception {
+        AirConditionerState airState = new AirConditionerState(true, 23);
+        HumidityState humState = new HumidityState(50);
+
+        updateTargetState(
+                this.onboardedApi.getTarget(),
+                new TargetState[]{airState, humState});
+        Thread.sleep(1000);
+
+        Map results = onboardedApi.getTargetState();
+
+        Assert.assertNotNull(results);
+        Assert.assertEquals(2, results.size());
+        Assert.assertEquals(airState, (AirConditionerState) results.get(ALIAS1));
+        Assert.assertEquals(humState, (HumidityState) results.get(ALIAS2));
+
+        AirConditionerState gotAirSteta = onboardedApi.getTargetState(ALIAS1,
+                AirConditionerState.class);
+        Assert.assertEquals(airState, gotAirSteta);
+
+        HumidityState gotHumSteta = onboardedApi.getTargetState(ALIAS2,
+                HumidityState.class);
+        Assert.assertEquals(humState, gotHumSteta);
+    }
+}
--- thingiftest/src/androidTest/java/com/kii/thingif/states/HumidityState.java
+++ thingiftest/src/androidTest/java/com/kii/thingif/states/HumidityState.java
@@ -2,8 +2,15 @@ package com.kii.thingif.states;
 
 import com.kii.thingif.TargetState;
 
-public class HumidityState implements TargetState {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class HumidityState implements TargetState, StateToJSON {
     public int currentHumidity;
+
+    public HumidityState(int currentHumidity) {
+        this.currentHumidity = currentHumidity;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -15,5 +22,15 @@ public class HumidityState implements TargetState {
         }
         HumidityState other = (HumidityState)o;
         return this.currentHumidity == other.currentHumidity;
+    }
+
+    @Override
+    public JSONObject toJSONObject() {
+        try {
+            return new JSONObject()
+                    .putOpt("currentHumidity", this.currentHumidity);
+        }catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
