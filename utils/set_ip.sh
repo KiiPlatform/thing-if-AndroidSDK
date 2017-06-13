2d406d3b8b7ea9d94530043f7a3d1253f9b9e015
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-06-13 18:13:22 +0900

    Fix set_ip.sh.

--- utils/set_ip.sh
+++ utils/set_ip.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-IP_ADDR=`command -v ip &> /dev/null && \
- ip addr show eth0 | sed -nEe 's/^\s+inet\W+([0-9.]+).*$/\1/p' || \
- ifconfig en0 | sed -nEe 's/^[[:space:]]+inet[[:space:]]+([0-9.]+).*$/\1/p'`
+if command -v ip &> /dev/null; then
+    IP_ADDR=`ip addr show eth0 | sed -nEe 's/^\s+inet\W+([0-9.]+).*$/\1/p'`
+elif command -v ifconfig &> /dev/null; then
+    IP_ADDR=`ifconfig en0 | sed -nEe 's/^[[:space:]]+inet[[:space:]]+([0-9.]+).*$/\1/p'`
+else
+    echo "command not found"
+    exit 1
+fi
 
 mkdir -p thingif/src/test/assets
 echo "{\"IP\" : \"${IP_ADDR}\"}" > thingif/src/test/assets/setting.json
