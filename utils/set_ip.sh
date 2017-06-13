ae840e8bfc099d8a13ebd56f910567fc6cdce455
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-06-13 17:42:18 +0900

    Make a directory before create setting.json.

--- utils/set_ip.sh
+++ utils/set_ip.sh
@@ -4,4 +4,5 @@ IP_ADDR=`command -v ip &> /dev/null && \
  ip addr show eth0 | sed -nEe 's/^\s+inet\W+([0-9.]+).*$/\1/p' || \
  ifconfig en0 | sed -nEe 's/^[[:space:]]+inet[[:space:]]+([0-9.]+).*$/\1/p'`
 
+mkdir -p thingif/src/test/assets
 echo "{\"IP\" : \"${IP_ADDR}\"}" > thingif/src/test/assets/setting.json
